53065f98e78b2e55df054eb0e9819acafa9c80f0
ShodaPratheepKumarReddy <79445826+ShodaPratheepKumarReddy@users.noreply.github.com>
2021-04-30 12:10:56 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,10 +1,7 @@
 /**
  * @file main.c
  * @author 255967 Shoda Pratheep Kumar Reddy
- * @brief  Seat Heating Application
- * @brief when both seat and heat button are ON then we are going to read the analog value from the sensor.
- * @brief Then we are Converting Analog value to Digital value.
- * @brief Then generating PWM wave form with their respective duty cycle.And displaying the respective temperature.
+ * @brief  Seat Heating Application.when both seat and heat button are ON then we are going to read the analog value from the sensor.Then we are Converting Analog value to Digital value.Then generating PWM wave form with their respective duty cycle.And displaying the respective temperature.
  * @version 0.1
  * @date 2021-04-28
  *
@@ -27,17 +24,15 @@ int main(void)
             if(HeaterButton_ON)  //Checking Heater Button is ON or OFF.
             {                     //If both are ON  then enter into this if loop.
                 change_led_state(LED_ON);
+                digital_temp_value=ReadADC(0); //Analog to Digital Conversion.
+                generate_pwm(digital_temp_value);  //Generating the respective PWM wave form.
+                uart_transmit(digital_temp_value);    //Transmiting the Temperature value using UART protocol.
                 delay(LED_ON_TIME);
+            }
+            else
+            {
                 change_led_state(LED_OFF);
                 delay(LED_OFF_TIME);
-
-
-                while(1)
-                {
-                   digital_temp_value=ReadADC(0); //Analog to Digital Conversion.
-                   generate_pwm(digital_temp_value);  //Generating the respective PWM wave form.
-                   uart_transmit(digital_temp_value);    //Transmiting the Temperature value using UART protocol.
-                }
             }
         }
         else
